c56a3043ca584d47268c161bbc28a7909c3f4071
Daniel Ortiz Castro <dqniort@gmail.com>
2018-10-17 17:46:08 -0500

    Navbar run!
    La barra de navegacion ya esta funcionando.
    Continuara...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { DeudoresComponent } from './componentes/deudores/deudores.component';
+import { AlmacenComponent } from './componentes/almacen/almacen.component';
+import { HomeComponent } from './componentes/home/home.component';
+
+
+const
+routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'home', component: HomeComponent } ,
+{ path: 'deudores', component: DeudoresComponent } ,
+{ path: 'almacen', component: AlmacenComponent },
+{ path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DeudoresComponent,
+    AlmacenComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes
+)
   ],
   providers: [],
   bootstrap: [AppComponent]
