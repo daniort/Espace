42683bb27baed8b7f2e15b827e26461f0de0426c
Daniel Ortiz Castro <dqniort@gmail.com>
2018-10-24 19:09:11 -0500

    Sin home
    quite el home del navbar, por que no creo terminarlo

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductoInterface } from '../../interfaces/productointerface';
 import { CarritoInterface } from '../../interfaces/carritointerface';
 import { HomeService } from '../../servicios/home.service';
-import { Routes, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -23,38 +23,21 @@ total:number;
          vuelto:0,
          total:0,
          deuda:false,
-         vendedor:'admin'
-       };
-       productoelegido:ProductoInterface={
+         vendedor:'admin',
+ };
+ productoelegido:ProductoInterface={
                nombre:'',
                cantidad:0,
-      };
+};
 
 
-  constructor(public HomeService: HomeService, public router: Routes) {}
+  constructor(public HomeService: HomeService) {}
    ngOnInit() {
     this.HomeService.getProducts().subscribe(producto =>{
       this.productoitem=producto;
      console.log(this.productoitem);
     });
   }
-  onEscribiendo(){
-    console.log(this.productoelegido.nombre);
 
-    for (let i = 0; i < this.productoitem.length; i++) {
-
-        this.productoitem[i];
-
-    }
-  }
-  onAddCar(){
-    
-//    console.log(this.productoelegido.nombre);
-    //for(var i = 0;i<this.productoitem.length;i++) {
-      //return this.productoitem[i];
-//}
-    console.log("tu");
-//
-  }
 
 }
